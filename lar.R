997c46fe2004c3275ca30a0997f68bfb554325d0
unknown <lars1a9@gmail.com>
2016-03-30 14:35:32 +0300

    обновленный

--- lar.R
+++ lar.R
@@ -11,23 +11,33 @@ eddypro2=na.exclude(eddypro)
 cor.matrix = round(cor(eddypro2[, c(-1, -2, -3, -4)]), 2)
 model1=lm(RH~(rand_err_Tau+H+rand_err_H+LE+qc_LE+rand_err_LE+h2o_flux+co2_molar_density+co2_mole_fraction)^9,data=eddypro)
 model1
+#в первую модель было выбранно более значимые факторы
 summary(model1)
 model2=lm(RH~(LE+rand_err_LE+h2o_flux+rand_err_h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume+es)^8,data=eddypro)
 summary(model2)
+#после было убранно наименее значимый фактор
 model3=lm(RH~(LE+h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume+es)^6,data=eddypro)
 summary(model3)
+#после было убранно наименее значимый фактор
 model4=lm(RH~(LE+h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume)^5,data=eddypro)
 summary(model4)
 anova(model4)
 anova
+#был проведен анализ anova и после было убранно группа факторов которые между собой давали высокую значимость
 model5=lm(RH~(LE+h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume)^5 -LE:h2o_flux:co2_mole_fraction:air_temperature:air_molar_volume-LE:h2o_flux:co2_mole_fraction:air_molar_volume-LE:co2_mole_fraction:air_molar_volume-h2o_flux:air_molar_volume,data=eddypro)
 summary(model5)
 anova(model5)
+#было убранно группа факторов которые между собой давали высокую значимость
 model6=lm(RH~(LE+h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume)^5 -LE:h2o_flux:co2_mole_fraction:air_temperature:air_molar_volume-LE:h2o_flux:co2_mole_fraction:air_molar_volume-LE:co2_mole_fraction:air_molar_volume-h2o_flux:air_molar_volume-LE:co2_mole_fraction:air_temperature:air_molar_volume-h2o_flux:co2_mole_fraction:air_molar_volume-LE:h2o_flux:co2_mole_fraction-LE:co2_mole_fraction,data=eddypro)
 anova(model6)
+#было убранно группа факторов которые между собой давали высокую значимость
 model7=lm(RH~(LE+h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume)^5 -LE:h2o_flux:co2_mole_fraction:air_temperature:air_molar_volume-LE:h2o_flux:co2_mole_fraction:air_molar_volume-LE:co2_mole_fraction:air_molar_volume-h2o_flux:air_molar_volume-LE:co2_mole_fraction:air_temperature:air_molar_volume-h2o_flux:co2_mole_fraction:air_molar_volume-LE:h2o_flux:co2_mole_fraction-LE:co2_mole_fraction-h2o_flux:co2_mole_fraction-LE:air_temperature:air_molar_volume,data=eddypro)
 anova(model7)
+#было убранно группа факторов которые между собой давали высокую значимость
 model8=lm(RH~(LE+h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume)^5 -LE:h2o_flux:co2_mole_fraction:air_temperature:air_molar_volume-LE:h2o_flux:co2_mole_fraction:air_molar_volume-LE:co2_mole_fraction:air_molar_volume-h2o_flux:air_molar_volume-LE:co2_mole_fraction:air_temperature:air_molar_volume-h2o_flux:co2_mole_fraction:air_molar_volume-LE:h2o_flux:co2_mole_fraction-LE:co2_mole_fraction-h2o_flux:co2_mole_fraction-LE:air_temperature:air_molar_volume-h2o_flux:air_temperature:air_molar_volume-LE:co2_mole_fraction:air_temperature,data=eddypro)
 anova(model8)
+#было убранно группа факторов которые между собой давали высокую значимость
 model9=lm(RH~(LE+h2o_flux+co2_mole_fraction+air_temperature+air_molar_volume)^5 -LE:h2o_flux:co2_mole_fraction:air_temperature:air_molar_volume-LE:h2o_flux:co2_mole_fraction:air_molar_volume-LE:co2_mole_fraction:air_molar_volume-h2o_flux:air_molar_volume-LE:co2_mole_fraction:air_temperature:air_molar_volume-h2o_flux:co2_mole_fraction:air_molar_volume-LE:h2o_flux:co2_mole_fraction-LE:co2_mole_fraction-h2o_flux:co2_mole_fraction-LE:air_temperature:air_molar_volume-h2o_flux:air_temperature:air_molar_volume-LE:co2_mole_fraction:air_temperature-h2o_flux:co2_mole_fraction:air_temperature-LE:h2o_flux:co2_mole_fraction:air_temperature-h2o_flux:co2_mole_fraction:air_temperature:air_molar_volume,data=eddypro)
+anova(model9)
+#оканчательный вариант
 anova(model9)
